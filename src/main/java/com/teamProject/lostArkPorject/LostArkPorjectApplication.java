f8102f960db0feda2aac7792baef68fbf1089657
intellij-Laptop <ysh062100@gmail.com>
2024-08-09 10:55:43 +0900

    First commit

---
+++ src/main/java/com/teamProject/lostArkPorject/LostArkPorjectApplication.java
@@ -0,0 +1,13 @@
+package com.teamProject.lostArkPorject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostArkPorjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostArkPorjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamProject/lostArkPorject/LostArkPorjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamProject.lostArkPorject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostArkPorjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
