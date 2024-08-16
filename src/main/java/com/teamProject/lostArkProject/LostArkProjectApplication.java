f005155672bd397061e71908a9496f540eb1379f
intellij-Laptop <ysh062100@gmail.com>
2024-08-16 16:48:36 +0900

    Project name error modify

--- src/main/java/com/teamProject/lostArkProject/LostArkProjectApplication.java
+++ src/main/java/com/teamProject/lostArkProject/LostArkProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LostArkPorjectApplication {
+public class LostArkProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LostArkPorjectApplication.class, args);
+		SpringApplication.run(LostArkProjectApplication.class, args);
 	}
 
 }
