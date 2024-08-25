73bce3492a35fc6651611e4f6d652a97bd0173cc
BY-ee <gjwjqgh3@naver.com>
2024-08-25 22:39:59 +0900

    add: 입력받은 닉네임으로 유저 정보를 가져오는 로직 구현

---
+++ src/main/java/com/teamProject/lostArkProject/domain/CharacterInfo.java
@@ -0,0 +1,22 @@
+package com.teamProject.lostArkProject.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CharacterInfo {
+    @JsonProperty("ServerName")
+    private String serverName;
+    @JsonProperty("CharacterName")
+    private String characterName;
+    @JsonProperty("CharacterLevel")
+    private int characterLevel;
+    @JsonProperty("CharacterClassName")
+    private String characterClassName;
+    @JsonProperty("ItemAvgLevel")
+    private String itemAvgLevel;
+    @JsonProperty("ItemMaxLevel")
+    private String itemMaxLevel;
+}
