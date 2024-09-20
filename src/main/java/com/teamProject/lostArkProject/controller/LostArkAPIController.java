859972e75dcdc7c758aa43bdfbf53aa6736e86cd
BY-ee <gjwjqgh3@naver.com>
2024-09-20 14:03:08 +0900

    fix: 내실 차트에 사용하는 데이터를 제대로 가져오지 못하는 문제 수정

--- src/main/java/com/teamProject/lostArkProject/controller/LostArkAPIController.java
+++ src/main/java/com/teamProject/lostArkProject/controller/LostArkAPIController.java
@@ -2,7 +2,9 @@ package com.teamProject.lostArkProject.controller;
 
 import com.teamProject.lostArkProject.domain.Calendar;
 import com.teamProject.lostArkProject.domain.CharacterInfo;
+import com.teamProject.lostArkProject.domain.CollectibleItem;
 import com.teamProject.lostArkProject.service.LostArkAPIService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,12 @@ public class LostArkAPIController {
     public Mono<Map<String, Calendar>> getRemainTimes() {
         return lostArkAPIService.getCalendar()
                 .flatMap(calendar -> lostArkAPIService.getRemainTimes());
+    }
+
+    // collectibles url로 GET 요청 시 json 데이터 반환
+    @GetMapping("/collectibles")
+    public Mono<List<CollectibleItem>> getCharacterCollectable(HttpSession session) {
+        String characterName = (String) session.getAttribute("nickname"); // http 세션에서 가져온 닉네임
+        return lostArkAPIService.getCharacterCollectible(characterName);
     }
 }
