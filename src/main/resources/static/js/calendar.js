2cb87b2854cf8b06f778b08db3703bf1fdd7e217
BY-ee <gjwjqgh3@naver.com>
2024-09-26 20:12:49 +0900

    add: 메인 페이지의 Remain Time에서 Show All 링크 클릭하면 modal 띄우고 남은 시간 출력

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,3 +1,42 @@
+/********************
+ * modal 관련 로직 
+ *******************/
+// 웹페이지 로드 후 알림 설정 modal 로직을 동적으로 추가
+(($) => {
+    'use strict';
+    const modalHTML = `
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content" style="background-color: #4f4f4f;">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">알림 설정</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" id="remain-time-modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+                        <button type="button" class="btn btn-primary">저장</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    $('body').append(modalHTML);
+})(jQuery);
+
+// Show all 버튼 클릭할 시 modal 창 띄움
+$('#remain-time-list').on('click', (event) => {
+    event.preventDefault();
+    $('#staticBackdrop').modal('show');
+});
+
+
+/**************************
+ * remain time 관련 로직
+ *************************/
 $(document).ready(function() {
     fetchCalendar();
 
@@ -23,11 +62,32 @@ function fetchCalendar() {
 
 // Remain Time 함수
 function updateRemainTime(remainTimes) {
-    let calendarDiv = $('#calendar');
-    calendarDiv.empty();
+    // 메인 페이지의 Remain Time 칸에 출력되는 남은 시간
+    const $calendarDiv = $('#calendar');
+    $calendarDiv.empty();
 
     Object.entries(remainTimes).forEach(([key, value]) => {
-        calendarDiv.append(`
+        $calendarDiv.append(`
+            <div class="d-flex border-bottom py-3">
+                <div class="w-100 ms-3">
+                    <div class="d-flex">
+                        <img class="rounded-circle flex-shrink-0" src="${value.ContentsIcon}" alt="" style="width: 40px; height: 40px;">
+                        <div class="text-start ms-3">
+                            <h6 class="mb-0">${key} - ${value.ContentsName}</h6>
+                            <small>Remain Time: ${value.remainTime}</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+
+    // Remain Time의 Show All 클릭하면 나오는 modal에 출력되는 남은 시간
+    const $remainTimeBody = $('#remain-time-modal-body');
+    $remainTimeBody.empty();
+
+    Object.entries(remainTimes).forEach(([key, value]) => {
+        $remainTimeBody.append(`
             <div class="d-flex border-bottom py-3">
                 <div class="w-100 ms-3">
                     <div class="d-flex">
