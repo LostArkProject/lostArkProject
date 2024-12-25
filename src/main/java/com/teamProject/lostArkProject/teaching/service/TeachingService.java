00c40a952a5e21ed893f9ea37f212e09a325c277
lililuna <wendy19991018@gmail.com>
2024-12-25 18:24:43 +0900

    mentor/mentee 신청부분 관련 구현

--- src/main/java/com/teamProject/lostArkProject/teaching/service/TeachingService.java
+++ src/main/java/com/teamProject/lostArkProject/teaching/service/TeachingService.java
@@ -1,10 +1,18 @@
 package com.teamProject.lostArkProject.teaching.service;
 
+import com.teamProject.lostArkProject.teaching.dto.MenteeApplyDTO;
 import com.teamProject.lostArkProject.teaching.dto.MenteeDTO;
 import com.teamProject.lostArkProject.teaching.dto.MentorDTO;
+
+import java.util.List;
+import java.util.Map;
 
 
 public interface TeachingService {
     public void newMentor(MentorDTO mentorDTO);
     public void newMentee(MenteeDTO menteeDTO);
+    public void newMenteeApply(MenteeApplyDTO menteeApplyDTO);
+    public List<Map<String,Object>> getMentorList();
+    public Map<String, Object> getMentorListDetail(long mentorId);
+
 }
