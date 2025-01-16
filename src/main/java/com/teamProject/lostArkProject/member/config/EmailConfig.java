0643ac429b16ccf38406c1b294b18159b6e60080
intellij-Laptop <ysh062100@gmail.com>
2025-01-16 15:42:03 +0900

    fix[config] : 앱 비밀번호 변경 및 환경 변수 처리

--- src/main/java/com/teamProject/lostArkProject/member/config/EmailConfig.java
+++ src/main/java/com/teamProject/lostArkProject/member/config/EmailConfig.java
@@ -3,20 +3,25 @@ package com.teamProject.lostArkProject.member.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:config.properties")
 public class EmailConfig {
+    @Value("${emailPassword}")
+    private String emailPassword;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("ysh0621000@gmail.com");
-        mailSender.setPassword("wipf eitu qsir ompf");
+        mailSender.setPassword(emailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
